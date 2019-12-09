bd3fdd88d6a7438cf8fd55759ee057fb007ac0ee
IvanGarL <if.garcia@uniandes.edu.co>
2019-12-09 00:02:28 -0500

    pwa not working *sad music playing background*

--- car-guide-back/car-guide-front/src/Components/Forbidden/Forbidden.js
+++ car-guide-back/car-guide-front/src/Components/Forbidden/Forbidden.js
@@ -14,127 +14,127 @@ class Forbidden extends Component {
     render(){
         return(
         <div className="top_spaced">
-            <div class="wrapper">
-                <section class="pedastal">
-                    <div class="pedastal-block1"></div>
-                    <div class="pedastal-block2"></div>
-                    <div class="trapezium">
+            <div className="wrapper">
+                <section className="pedastal">
+                    <div className="pedastal-block1"></div>
+                    <div className="pedastal-block2"></div>
+                    <div className="trapezium">
                         <div></div>
                         <div></div>
                     </div>
                 </section>
-                <section class="hall">
-                    <div class="hall-pillar"></div>
-                    <div class="hall-support"></div>
-                    <div class="hall-pillar"></div>
-                    <div class="hall-support"></div>
-                    <div class="hall-pillar"></div>
-                    <div class="hall-support"></div>
-                    <div class="hall-pillar"></div>
-                    <div class="hall-support"></div>
-                    <div class="hall-pillar"></div>
-                    <div class="hall-support"></div>
-                    <div class="hall-pillar"></div>
-                    <div class="hall-support"></div>
-                    <div class="hall-pillar"></div>
-                    <div class="hall-support"></div>
-                    <div class="hall-pillar"></div>
-                    <div class="hall-support"></div>
-                    <div class="hall-pillar"></div>
-                    <div class="hall-support"></div>
-                    <div class="hall-pillar"></div>
+                <section className="hall">
+                    <div className="hall-pillar"></div>
+                    <div className="hall-support"></div>
+                    <div className="hall-pillar"></div>
+                    <div className="hall-support"></div>
+                    <div className="hall-pillar"></div>
+                    <div className="hall-support"></div>
+                    <div className="hall-pillar"></div>
+                    <div className="hall-support"></div>
+                    <div className="hall-pillar"></div>
+                    <div className="hall-support"></div>
+                    <div className="hall-pillar"></div>
+                    <div className="hall-support"></div>
+                    <div className="hall-pillar"></div>
+                    <div className="hall-support"></div>
+                    <div className="hall-pillar"></div>
+                    <div className="hall-support"></div>
+                    <div className="hall-pillar"></div>
+                    <div className="hall-support"></div>
+                    <div className="hall-pillar"></div>
                 </section>
-                <section class="lower-support">
-                    <div class="lower-support-pillar"></div>
-                    <div class="ornaments">
+                <section className="lower-support">
+                    <div className="lower-support-pillar"></div>
+                    <div className="ornaments">
                         <div></div>
                         <div></div>
                     </div>
-                    <div class="lower-support-pillar"></div>
-                    <div class="ornaments">
+                    <div className="lower-support-pillar"></div>
+                    <div className="ornaments">
                         <div></div>
                         <div></div>
                     </div>
-                    <div class="lower-support-pillar"></div>
-                    <div class="ornaments">
+                    <div className="lower-support-pillar"></div>
+                    <div className="ornaments">
                         <div></div>
                         <div></div>
                     </div>
-                    <div class="lower-support-pillar"></div>
-                    <div class="ornaments">
+                    <div className="lower-support-pillar"></div>
+                    <div className="ornaments">
                         <div></div>
                         <div></div>
                     </div>
-                    <div class="lower-support-pillar"></div>
-                    <div class="ornaments">
+                    <div className="lower-support-pillar"></div>
+                    <div className="ornaments">
                         <div></div>
                         <div></div>
                     </div>
-                    <div class="lower-support-pillar"></div>
-                    <div class="ornaments">
+                    <div className="lower-support-pillar"></div>
+                    <div className="ornaments">
                         <div></div>
                         <div></div>
                     </div>
-                    <div class="lower-support-pillar"></div>
-                    <div class="ornaments">
+                    <div className="lower-support-pillar"></div>
+                    <div className="ornaments">
                         <div></div>
                         <div></div>
                     </div>
-                    <div class="lower-support-pillar"></div>
-                    <div class="ornaments">
+                    <div className="lower-support-pillar"></div>
+                    <div className="ornaments">
                         <div></div>
                         <div></div>
                     </div>
-                    <div class="lower-support-pillar"></div>
-                    <div class="ornaments">
+                    <div className="lower-support-pillar"></div>
+                    <div className="ornaments">
                         <div></div>
                         <div></div>
                     </div>
-                    <div class="lower-support-pillar"></div>
+                    <div className="lower-support-pillar"></div>
                 </section>
-                <section class="lower-roof">
+                <section className="lower-roof">
                     <div></div>
                     <div></div>
                 </section>
-                <section class="upper-support">
-                    <div class="container">
-                        <div class="lower-support-pillar"></div>
-                        <div class="lower-support-pillar"></div>
-                        <div class="lower-support-pillar"></div>
-                        <div class="lower-support-pillar"></div>
-                        <div class="lower-support-pillar"></div>
-                        <div class="lower-support-pillar"></div>
+                <section className="upper-support">
+                    <div className="container">
+                        <div className="lower-support-pillar"></div>
+                        <div className="lower-support-pillar"></div>
+                        <div className="lower-support-pillar"></div>
+                        <div className="lower-support-pillar"></div>
+                        <div className="lower-support-pillar"></div>
+                        <div className="lower-support-pillar"></div>
                     </div>
-                    <div class="ornaments"><div></div><div></div></div>
-                    <div class="ornaments"><div></div><div></div></div>
-                    <div class="ornaments"><div></div><div></div></div>
-                    <div class="ornaments"><div></div><div></div></div>
-                    <div class="ornaments"><div></div><div></div></div>
-                    <div class="ornaments"><div></div><div></div></div>
-                    <div class="ornaments"><div></div><div></div></div>
-                    <section class="sign"></section>
+                    <div className="ornaments"><div></div><div></div></div>
+                    <div className="ornaments"><div></div><div></div></div>
+                    <div className="ornaments"><div></div><div></div></div>
+                    <div className="ornaments"><div></div><div></div></div>
+                    <div className="ornaments"><div></div><div></div></div>
+                    <div className="ornaments"><div></div><div></div></div>
+                    <div className="ornaments"><div></div><div></div></div>
+                    <section className="sign"></section>
                 </section>
-                <section class="upper-roof">
+                <section className="upper-roof">
                     <div></div>
                     <svg width="520px" height="90px">
                         <path d="M495.689265,72.9065145 L520,90 L0,90 L24.3069308,72.9091893 L45.9698498,74.0444947 C88.9890231,76.2990341 125.690619,43.2527689 127.945158,0.233595624 L127.687016,0.220066965 L128,0 L392,0 L392.30918,0.217392187 L392,0.233595624 C394.254539,43.2527689 430.956135,76.2990341 473.975308,74.0444947 L495.689265,72.9065145 Z" id="Combined-Shape" fill="#FDBB3B"></path>
                     </svg>
                     <div></div>
                 </section>
-                <section class="roof-top">
+                <section className="roof-top">
                     <div></div>
                     <div></div>
                 </section>
-                <div class="cloud cloud-01"></div>
-                <div class="cloud cloud-02"></div>
-                <div class="cloud cloud-03"><div></div></div>
-                <div class="sun"><div class="copy">403</div></div>
+                <div className="cloud cloud-01"></div>
+                <div className="cloud cloud-02"></div>
+                <div className="cloud cloud-03"><div></div></div>
+                <div className="sun"><div className="copy">403</div></div>
             </div>
-            <div class="wall">
-                    <div class="wall-roofing-bottom"></div>
-                    <div class="wall-roofing-top"></div>
+            <div className="wall">
+                    <div className="wall-roofing-bottom"></div>
+                    <div className="wall-roofing-top"></div>
             </div>
-            <div class="headline">
+            <div className="headline">
                 <h1>Forbidden City</h1>
                 <h2>✋ You don't have permission to access 🚧 this area ✋</h2>
             </div>
